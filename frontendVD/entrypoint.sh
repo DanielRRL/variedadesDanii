8d91060d12e2fc6a4956bf283120a36c484277d1
DanielRRL <reinaramon289@gmail.com>
2026-06-04 20:12:56 -0500

    correcion de errores de nginx

--- frontendVD/entrypoint.sh
+++ frontendVD/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
+export PORT=${PORT:-80}
 if [ -n "$API_URL" ]; then
   find /usr/share/nginx/html -name "*.js" -exec sed -i "s|__VITE_API_URL__|${API_URL}|g" {} \;
 fi
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /tmp/default.conf
+mv /tmp/default.conf /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
