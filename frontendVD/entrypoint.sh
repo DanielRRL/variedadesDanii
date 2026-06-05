3d3384febb1bc23846b2eb0e59966d36f265bfb3
DanielRRL <reinaramon289@gmail.com>
2026-06-05 11:06:26 -0500

    fix: usa placeholder NGINX_PORT para evitar doble reemplazo

--- frontendVD/entrypoint.sh
+++ frontendVD/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 export PORT=${PORT:-80}
-# Reemplaza API URL en los archivos JS del build
 find /usr/share/nginx/html -name "*.js" -exec sed -i "s|VITE_API_URL_PLACEHOLDER|${API_URL}|g" {} \;
-# Reemplaza el puerto en nginx.conf
-sed -i "s/listen \${PORT}/listen ${PORT}/g" /etc/nginx/conf.d/default.conf
-sed -i "s/listen 80/listen ${PORT}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/NGINX_PORT/${PORT}/g" /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
